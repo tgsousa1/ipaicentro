72b29d9b094a444a97c7012c4856facdf693ed82
tgsousa1 <49939294+tgsousa1@users.noreply.github.com>
2020-09-25 16:04:57 +0100

    Update basemap

--- map.js
+++ map.js
@@ -1,10 +1,16 @@
 const map = L.map('map').setView([40.007713,-7.9091],7.5)
 
-const Stadia_AlidadeSmooth = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png', {
+/*const Stadia_AlidadeSmooth = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png', {
 	maxZoom: 20,
 	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
 });
-Stadia_AlidadeSmooth.addTo(map);
+Stadia_AlidadeSmooth.addTo(map);*/
+
+const OpenStreetMap_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
+OpenStreetMap_Mapnik.addTo(map)
 
 /* const Stadia_OSMBright = L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png', {
 	maxZoom: 20,
